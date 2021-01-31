70c0ecdbc49fe2f79693de3d7ba8f2face4febef
naufalibn24 <naufalibnu75@gmail.com>
2021-02-01 04:49:07 +0700

    adding database

--- config/Database.js
+++ config/Database.js
@@ -4,4 +4,13 @@ const conn = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
+  database: "sea_db",
+  port: "3306",
 });
+
+conn.connect((err) => {
+  if (err) throw err;
+  console.log("Connected with MySql");
+});
+
+module.exports = conn;
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const conn = require("./config/Database");
 const app = express();
 
 const port = 3000;
+
+conn;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
